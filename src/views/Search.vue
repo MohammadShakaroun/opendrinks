6a20e2aed44f2d4a338350172cae0ed445e20368
MohammadShakaroun <aucemohamadchakaroun@gmail.com>
2023-09-27 12:01:11 +0300

    generate opendrinks app

---
+++ src/views/Search.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="search container">
+    <h2 class="text-center">{{ $t('search.findYourDrink') }}</h2>
+    <RecipeFind />
+  </div>
+</template>
+
+<script>
+import RecipeFind from '@/components/RecipeFind.vue';
+
+export default {
+  name: 'search',
+  components: {
+    RecipeFind,
+  },
+};
+</script>
